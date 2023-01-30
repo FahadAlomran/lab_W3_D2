a9cff56fa46427aeaecae4ce3c8eec55d6777a5e
Fahad Al Orman <fahadalomran19@gmail.com>
2023-01-30 15:03:18 +0300

    lab solved

---
+++ main.js
@@ -0,0 +1,93 @@
+// let root= document.getElementById('root');
+//  let div =document.createElement('div')
+//  div.innerHTML='hello'
+//  root.appendChild(div)
+
+// let cont = document.getElementById("root");
+// let elm = React.createElement("div", {
+//   children: "fahad",
+// });
+
+// ReactDOM.render(elm, cont);
+
+// let root = document.getElementById("root");
+
+// let element = React.createElement("div", {
+//     children: "HATIM"
+// })
+
+//
+let root = document.getElementById("root");
+let element = 
+  <div id ="div">
+  
+   <div id ="CONTACT">
+
+    <h2>CONTACT:</h2>
+    
+    <ul >
+    <li>Riyadh</li>
+    <li>fahadalomran19@gmail.c
+om
+</li>
+    <li>Saudi Arabia - Riyadh -
+ALmlqa district
+</li>
+    </ul>
+
+
+<h2>LANGUAGES:</h2>
+<ul>
+    <li>Arabic</li>
+    <li>English</li>
+    </ul>
+
+    <h2>SKILLS:</h2>
+
+    <ul>
+    <li>Team work</li>
+    <li>Time management</li>
+    <li>Problem Solving</li>
+    <li>SDLC</li>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JAVASCRIPT</li>
+    <li>NodeJS</li>
+    <li>MongoDB</li>
+    <li> SQL</li>
+    </ul>
+    </div>
+
+     <div id="m" >
+<h1>FAHAD ALOMRAN</h1>
+<hr></hr>
+<h2>COMPUTER SCIENCE</h2>
+
+<p>Saudi who graduated from King Saud University with a basic knowledge of
+computer science theories , principles and standards . Looking for a
+challenging opportunity to work in reputable company with a powerful
+communication and leadership skills .Hardworking and eager to get
+opportunities to improve myself professionally and contribute to the growth
+of the organization with quality services</p>
+
+<h2>EDUCATION:</h2>
+
+<p>Bachelor of computer science
+King Saud University
+Saudi Arabia - Riyadh</p>
+
+<h2>PROJECTS:</h2>
+<p>LOCATION PRIVACY TECHNIQUE FOR INTERNET
+BOWSERS USING DATA ANONYMIZATION.</p>
+
+<h2>COURSE:</h2>
+
+<ul>
+    <li>THE WEB DEVELOPER BOOTCAMP 2021</li>
+    <li>WEBSITES DESIGN WITH HTML & CSS</li>
+    </ul>
+</div>
+  </div>
+
+ReactDOM.render(element, root);
+
